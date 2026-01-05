ef2d25ac78ed6d240c8823e1da481c35f60eee9d
Marlon M <lonmaniti@gmail.com>
2026-01-05 21:38:53 +1300

    chore: bump version to 0.8.1
    - Updated plugin version in index.ts to match package.json
    - Patch version for documentation improvement (ignore functionality guide)

--- src/linter/eslint-plugin/index.ts
+++ src/linter/eslint-plugin/index.ts
@@ -42,7 +42,7 @@ import formValidation from './rules/form-validation'
 const plugin: ESLint.Plugin = {
   meta: {
     name: 'test-a11y-js',
-    version: '0.8.0'
+    version: '0.8.1'
   },
   rules: {
     'image-alt': imageAlt,
